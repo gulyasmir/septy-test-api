281383480444df465f7f8775c47ea8d917999e6d
Gulnara <gulyasmir@yandex.ru>
2021-11-13 16:31:08 +0300

    fix

--- app/Http/Controllers/Api/Screen/ListScreen2Controller.php
+++ app/Http/Controllers/Api/Screen/ListScreen2Controller.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api\Screen;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\Controller;
 use App\Models\Models\ScreenModel;
 use Illuminate\Http\Request;
 
--- app/Http/Controllers/Api/Screen/ListScreen4Controller.php
+++ app/Http/Controllers/Api/Screen/ListScreen4Controller.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api\Screen;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\Controller;
 use App\Models\Models\ListScreen2Model;
 use Illuminate\Http\Request;
 use App\Models\Models\ListScreen4Model;
