e3424b3e7022f1304602c84149eb4c54f24efb58
Gulnara <gulyasmir@yandex.ru>
2021-11-19 23:24:27 +0300

    lll

--- app/Http/Controllers/Api/UploadFileController.php
+++ app/Http/Controllers/Api/UploadFileController.php
@@ -8,16 +8,19 @@ class UploadFileController extends Controller
 {
     public function upload(Request $request)
     {
-       // $uploadedFiles = [];
-     //   $file = $request->data->file;
-       // foreach ($request->file('files') as $file) {
-        //    $fileName = bcrypt(microtime()) . "." . $file->getClientOriginalExtension();
-       //     $file->move('/storage/images', $fileName);
-       //     array_push($uploadedFiles, "/storage/images/{$fileName}");
-      //  }
+        $uploadedFiles = [];
+
+        foreach ($request->file('files') as $file) {
+            $fileName = bcrypt(microtime()) . "." . $file->getClientOriginalExtension();
+            $file->move('/storage/images', $fileName);
+            array_push($uploadedFiles, "/storage/images/{$fileName}");
+        }
 
         return response($request);
 
+
     }
+
+
 }
 
