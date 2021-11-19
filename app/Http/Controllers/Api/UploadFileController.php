101fe023d9119dde0eb517fecd3ad28efaea5fda
gulyasmir <gulyasmir@yandex.ru>
2021-11-19 18:30:46 +0300

    upload image12eeqqqqq

--- app/Http/Controllers/Api/UploadFileController.php
+++ app/Http/Controllers/Api/UploadFileController.php
@@ -9,14 +9,14 @@ class UploadFileController extends Controller
     public function upload(Request $request)
     {
        // $uploadedFiles = [];
-        $file = $request->data->file;
+     //   $file = $request->data->file;
        // foreach ($request->file('files') as $file) {
         //    $fileName = bcrypt(microtime()) . "." . $file->getClientOriginalExtension();
        //     $file->move('/storage/images', $fileName);
        //     array_push($uploadedFiles, "/storage/images/{$fileName}");
       //  }
 
-        return response($file);
+        return response($request);
 
     }
 }
